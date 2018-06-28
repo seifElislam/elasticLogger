66deb9a54c11537540bf6ae1438e3c82a768da6b
seifElislam <seif.eleslam1990@gmail.com>
2018-06-28 14:12:54 +0200

    add app view, models, serializer

--- elasticLogger/app/models.py
+++ elasticLogger/app/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField(blank=True, default='')
+
+    class Meta:
+        ordering = ('created',)
---
+++ elasticLogger/app/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Snippet
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.save()
+        return instance
